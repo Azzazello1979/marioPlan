1b73fc028c37aacc942f4517396cd25047c00f88
Balint H <balint.haui@gmail.com>
2020-02-15 08:56:41 +0100

    Dashboard with ProjectList and Notifications comps added to App.js

---
+++ marioplan/src/components/dashboard/Notifications.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notifications = () => {
+    return(
+        <div>
+            <p1>Notifications</p1>
+        </div>
+    )
+}
+
+export default Notifications;
