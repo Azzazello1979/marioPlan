96a2c759d945d058d1e2dca35b47b1eca61c99a8
Balint H <balint.haui@gmail.com>
2020-02-15 16:40:42 +0100

    dashboard component successfully connected to Redux store

--- marioplan/src/components/dashboard/Dashboard.jsx
+++ marioplan/src/components/dashboard/Dashboard.jsx
@@ -3,14 +3,16 @@ import Notifications from './Notifications';
 import ProjectList from './../projects/ProjectList';
 import { connect } from 'react-redux';
 
+
 class Dashboard extends Component{
     render(){
-        console.log(this.props)
+        // console.log('the props of Dashboard: ', this.props)
+        const { projects } = this.props;
         return(
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6">
-                        <ProjectList />
+                        <ProjectList projects={ projects }/>
                     </div>
                     <div className="col s12 m5 offset-m1">
                         <Notifications />
@@ -21,10 +23,25 @@ class Dashboard extends Component{
     }
 }
 
-const mapStateToPropssss = state => {
+const stateInjector = state => {
+    // the state arg above is the state of the store
+    // map-state-to-props
+    // console.log('stateInjector functions arg:state ', state)
     return {
         projects: state.project.projects
     }
 }
 
-export default connect(mapStateToPropssss)(Dashboard);
+export default connect(stateInjector)(Dashboard);
+// The connect function of react-redux connects a react component 
+// with the redux store.
+// As args it takes a function that 'maps the state to props' as
+// well as the react component.
+
+// the stateInjector function above...
+
+// THE PROPS OF THE COMPONENT INHERITS THE STATE OF THE STORE
+
+// THE STATE OF THE STORE IS PLUGGED INTO THE PROPS OF THE COMPONENT
+
+// INJECTS THE STATE OF THE STORE INTO THE COMPONENT'S PROPS
--- marioplan/src/components/projects/ProjectSummary.jsx
+++ marioplan/src/components/projects/ProjectSummary.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import './ProjectSummary.css';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ project }) => {
   
   return (
     <div className="card project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Project Title</span>
+        <span className="card-title">{ project.title }</span>
         <p>Posted by Bruc</p>
         <p className="grey-text">3rd September, 2AM</p>
       </div>
