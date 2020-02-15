05f6c899d6382e0c2d4dc5182c0a643b1dde3852
Balint H <balint.haui@gmail.com>
2020-02-15 14:30:58 +0100

    redux fun begings ... creating rootReducer and lesser reducers

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+    projects: [
+        {id: '1', title: 'help me find peach', content: 'blahblabblah'},
+        {id: '2', title: 'collect all the stars', content: 'blahblabblah'},
+        {id: '3', title: 'egghunt with yoshi', content: 'blahblabblah'}
+    ]
 }
 
 const projectReducer = (state = initState, action) => {
--- marioplan/src/store/reducers/rootReducer.js
+++ marioplan/src/store/reducers/rootReducer.js
@@ -2,6 +2,10 @@ import authReducer from './authReducer';
 import projectReducer from './projectReducer';
 import { combineReducers } from 'redux';
 
+// this is the 'boss robot'(rootReducer) in front of the 
+// warehouse(store), 
+// giving orders(actions) to lesser robots(reducers)'
+
 const rootReducer = combineReducers({
     auth: authReducer,
     project: projectReducer
