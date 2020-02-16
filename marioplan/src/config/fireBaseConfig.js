119b9c30a69d02e1bf7bd5098ee720347217f22a
Balint H <balint.haui@gmail.com>
2020-02-16 09:36:22 +0100

    fireBase project made and config set up in config file

---
+++ marioplan/src/config/fireBaseConfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBm1kaCQFrvVOxS8MqAm6cIfnWqoupU0Hk",
+    authDomain: "marioplan-678df.firebaseapp.com",
+    databaseURL: "https://marioplan-678df.firebaseio.com",
+    projectId: "marioplan-678df",
+    storageBucket: "marioplan-678df.appspot.com",
+    messagingSenderId: "539796745467",
+    appId: "1:539796745467:web:6be12d34598aa22e4d545e",
+    measurementId: "G-9SSX4MVSYL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  firebase.firestore().settings({ timestampsInSnapshots: true });
+
+  export default firebase;
+  
