29bde60f2748c9342975748b71f4e90c0d2a624a
Balint H <balint.haui@gmail.com>
2020-02-25 16:30:51 +0100

    redirecting user to index after adding new project

--- marioplan/src/components/projects/ProjectSummary.jsx
+++ marioplan/src/components/projects/ProjectSummary.jsx
@@ -6,7 +6,9 @@ const ProjectSummary = ({ project }) => {
     <div className="card project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-        <p>Posted by Bruc</p>
+        <p>
+          posted by {project.authorFirstName} {project.authorLastName}
+        </p>
         <p className="grey-text">3rd September, 2AM</p>
       </div>
     </div>
