79e2bfd85761591bc3aadb0a8be18ed8d690ae2b
Balint H <balint.haui@gmail.com>
2020-02-26 17:16:52 +0100

    user joined the party not showing on new signup? firebase rules permission error?

--- marioplan/src/components/dashboard/Notifications.jsx
+++ marioplan/src/components/dashboard/Notifications.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { firestoreConnect } from 'react-redux-firebase';
+import { connect } from 'react-redux'; 
+import { compose } from 'redux';
+import moment from 'moment';
 
 const Notifications = props => {
+    const { notifications } = props;
+    console.log('notifications: ', notifications) ;
   return (
     <div className="section">
       <div className="card nice-card">
         <div className="card-content">
           <span className="card-title">Notifications</span>
           <ul className="notifications">
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
+            {
+                notifications && notifications.map(notification => {
+                    return(
+                        <li key={notification.id}>
+                            <span className="pink-text">{notification.user}</span>
+                            <span> {notification.content}</span>
+                            <div className="grey-text note-date">
+                                { moment(notification.time.toDate()).fromNow() }
+                            </div>
+                        </li>
+                    )
+                })
+            }
           </ul>
         </div>
       </div>
@@ -18,4 +33,17 @@ const Notifications = props => {
   );
 };
 
-export default Notifications;
+const stateInjector = state => {
+    return {
+        notifications: state.firestore.ordered.notifications
+    }
+}
+
+export default 
+compose(
+    connect(stateInjector, null),
+    firestoreConnect([
+        { collection: 'notifications', limit: 3 }
+    ])
+)
+(Notifications)
